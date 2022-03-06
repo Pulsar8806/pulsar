c8dfeb514a4ad0d9b58204fd0a6ca1081a6d79e9
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-06 20:32:22 +0300

    Update Downloader.py

--- Yukki/Core/PyTgCalls/Downloader.py
+++ Yukki/Core/PyTgCalls/Downloader.py
@@ -38,7 +38,7 @@ def download(videoid: str, mystic, title) -> str:
                 try:
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} İndiriyor 📥**\n\n**Başlık:** {title[:50]}:\n**Dosya Boyutlandırma 🗂️:** {size}\n\n**<u>İndirilen:</u>**\n**Hız:** {speed}\n**ETA:** {eta} Saniye\n\n\n{percentage} ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
                         )
                 except Exception as e:
                     pass
@@ -47,21 +47,21 @@ def download(videoid: str, mystic, title) -> str:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME}Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ███▓▓▓▓▓▓▓▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} İndiriyor 📥**\n\n**Başlık:** {title[:50]}:\n**Dosya Boyutlandırma 🗂️:** {size}\n\n**<u>İndirilen:</u>**\n**Hız:** {speed}\n**ETA:** {eta} Saniye\n\n\n{percentage} ███▓▓▓▓▓▓▓▓▓ 100%"
                         )
             if per > 500:
                 if flex[str(bytesx)] == 3:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ██████▓▓▓▓▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} İndiriyor 📥**\n\n**Başlık:** {title[:50]}:\n**Dosya Boyutlandırma 🗂️:** {size}\n\n**<u>İndirilen:</u>**\n**Hız:** {speed}\n**ETA:** {eta} Saniye\n\n\n{percentage} ██████▓▓▓▓▓▓ 100%"
                         )
             if per > 800:
                 if flex[str(bytesx)] == 4:
                     flex[str(bytesx)] += 1
                     if eta > 2:
                         mystic.edit(
-                            f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n**FileSize:** {size}\n\n**<u>Downloaded:</u>**\n**Speed:** {speed}\n**ETA:** {eta} Seconds\n\n\n{percentage} ██████████▓▓ 100%"
+                            f"**{MUSIC_BOT_NAME} İndiriyor**\n\n**Başlık:** {title[:50]}:\n**Dosya Boyutlandırma 🗂️:** {size}\n\n**<u>İndirilen:</u>**\n**Hız:** {speed}\n**ETA:** {eta} Saniye\n\n\n{percentage} ██████████▓▓ 100%"
                         )
         if d["status"] == "finished":
             try:
@@ -70,7 +70,7 @@ def download(videoid: str, mystic, title) -> str:
                 taken = "00:00"
             size = d["_total_bytes_str"]
             mystic.edit(
-                f"**{MUSIC_BOT_NAME} Downloader**\n\n**Title:** {title[:50]}:\n\n100% ████████████100%\n\n**Time Taken:** {taken} Seconds\n\nConverting Audio[FFmpeg Process]"
+                f"**{MUSIC_BOT_NAME} İndiriyor 📥**\n\n**Başlık:** {title[:50]}:\n\n100% ████████████100%\n\n**Geçen Süre ⏱️:** {taken} Saniye\n\nTalia Winamp 👉 [Dönüştürücü]"
             )
 
     ydl_optssx = {
