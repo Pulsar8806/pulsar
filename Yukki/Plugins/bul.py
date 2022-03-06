cd77d927ca364b88cf92ca950aa8439414f08934
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-07 02:30:22 +0300

    Update bul.py

--- Yukki/Plugins/bul.py
+++ Yukki/Plugins/bul.py
@@ -9,7 +9,7 @@ from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
 
 from Yukki import (BOT_USERNAME, app, db_mem) 
-from Datebase.filters import command
+from Datebase import filters command 
 
 
 def time_to_seconds(time):
