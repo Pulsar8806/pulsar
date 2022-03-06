d7dbeafb3c6f82676928b220b88969a1b3b3dc08
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-06 17:49:27 +0300

    Update directplay.py

--- Yukki/Plugins/directplay.py
+++ Yukki/Plugins/directplay.py
@@ -30,130 +30,7 @@ from Yukki.Utilities.videostream import start_stream_video
 from Yukki.Utilities.youtube import (get_yt_info_id, get_yt_info_query,
                                      get_yt_info_query_slider)
 
-from Yukki.Plugins.custom.func import mplay_stream, vplay_stream
-
-@app.on_message(
-    filters.command(["mplay", f"mplay@{BOT_USERNAME}"]) & filters.group
-)
-@checker
-@logging
-@PermissionCheck
-@AssistantAdd
-async def mplayaa(_, message: Message):    
-    await message.delete()
-    if message.chat.id not in db_mem:
-        db_mem[message.chat.id] = {}
-    if message.sender_chat:
-        return await message.reply_text(
-            " __Anonim Yönetici__ bu Sohbet Grubunda!\nYönetici Haklarından Kullanıcı Hesabına Geri Dön."
-        )
-    audio = (
-        (message.reply_to_message.audio or message.reply_to_message.voice)
-        if message.reply_to_message
-        else None
-    )
-    video = (
-        (message.reply_to_message.video or message.reply_to_message.document)
-        if message.reply_to_message
-        else None
-    )
-    url = get_url(message)
-    if audio:
-        mystic = await message.reply_text(
-            "🔄 Ses İşleniyor... Lütfen bekleyin!"
-        )
-        try:
-            read = db_mem[message.chat.id]["live_check"]
-            if read:
-                return await mystic.edit(
-                    "Canlı Yayın Oynatıyor... Müzik çalmak için durdurun"
-                )
-            else:
-                pass
-        except:
-            pass
-        if audio.file_size > 1073741824:
-            return await mystic.edit_text(
-                "Ses Dosyası Boyutu 150 MB'den Az Olmalıdır"
-            )
-        duration_min = seconds_to_min(audio.duration)
-        duration_sec = audio.duration
-        if (audio.duration) > DURATION_LIMIT:
-            return await mystic.edit_text(
-                f"**Süre Sınırı Aşıldı**\n\n**İzin Verilen Süre: **{DURATION_LIMIT_MIN} dakika\n**Alınan Süre:** {duration_min} dakikalar"
-            )
-        file_name = (
-            audio.file_unique_id
-            + "."
-            + (
-                (audio.file_name.split(".")[-1])
-                if (not isinstance(audio, Voice))
-                else "ogg"
-            )
-        )
-        file_name = path.join(path.realpath("downloads"), file_name)
-        file = await convert(
-            (await message.reply_to_message.download(file_name))
-            if (not path.isfile(file_name))
-            else file_name,
-        )
-        return await start_stream_audio(
-            message,
-            file,
-            "smex1",
-            "Telegram ile Verilen Ses",
-            duration_min,
-            duration_sec,
-            mystic,
-        )
-    elif video:
-        return await message.reply_text("Kullanmak /oynat veya /mplay başlatma komutları.")
-    elif url:
-        mystic = await message.reply_text("🔄 URL işleniyor... Lütfen bekleyin!")
-        if not message.reply_to_message:
-            query = message.text.split(None, 1)[1]
-        else:
-            query = message.reply_to_message.text
-        (
-            title,
-            duration_min,
-            duration_sec,
-            thumb,
-            videoid,
-            views, 
-            channel
-        ) = get_yt_info_query(query)
-        await mystic.delete()        
-        MusicData = f"Müzik Akışı {videoid}|{duration_min}|{message.from_user.id}"
-        return await mplay_stream(message,MusicData)
-    else:
-        if len(message.command) < 2:
-            buttons = playlist_markup(
-                message.from_user.first_name, message.from_user.id, "abcd"
-            )
-            await message.reply_photo(
-                photo="Utils/Playlist.jpg",
-                caption=(
-                    "**Kullanım:** /mplay [Müzik Adı veya Youtube Bağlantısı veya Sese Yanıt]\n\nÇalma Listelerini çalmak istiyorsanız! Aşağıdakilerden birini seçin."
-                ),
-                reply_markup=InlineKeyboardMarkup(buttons),
-            )
-            return
-        mystic = await message.reply_text("🔍 **Arıyorum**...")
-        query = message.text.split(None, 1)[1]
-        (
-            title,
-            duration_min,
-            duration_sec,
-            thumb,
-            videoid,
-            views, 
-            channel
-        ) = get_yt_info_query(query)
-        await mystic.delete()
-        MusicData = f"Müzik Akışı {videoid}|{duration_min}|{message.from_user.id}"
-        return await mplay_stream(message,MusicData)
-
+from Yukki.Plugins.custom.func import vplay_stream
 
 @app.on_message(
     filters.command(["vizle", f"vizle@{BOT_USERNAME}"]) & filters.group
@@ -243,7 +120,7 @@ async def vizle(_, message: Message):
             await message.reply_photo(
                 photo="Utils/Playlist.jpg",
                 caption=(
-                    "**Kullanım:** /oynat [Müzik Adı veya Youtube Bağlantısı veya Sese Yanıt]\n\nÇalma Listelerini çalmak istiyorsanız! Aşağıdakilerden birini seçin."
+                    "**Kullanım:** /vizle [Müzik Adı veya Youtube Bağlantısı veya Sese Yanıt]\n\nÇalma Listelerini çalmak istiyorsanız! Aşağıdakilerden birini seçin."
                 ),
                 reply_markup=InlineKeyboardMarkup(buttons),
             )
