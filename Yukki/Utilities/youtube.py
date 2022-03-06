5b48c45a353aa4985a4ec855d3e1668bb3cd4ee8
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-06 23:44:17 +0300

    Update youtube.py

--- Yukki/Utilities/youtube.py
+++ Yukki/Utilities/youtube.py
@@ -16,9 +16,7 @@ def get_yt_info_id(videoid):
             duration_sec = 0
         else:
             duration_sec = int(time_to_seconds(duration_min))
-        views = result["viewCount"]["short"]
-        channel = result["channel"]["name"]
-    return title, duration_min, duration_sec, thumbnail, views, channel
+    return title, duration_min, duration_sec, thumbnail
 
 
 def get_yt_info_query(query: str):
@@ -32,9 +30,7 @@ def get_yt_info_query(query: str):
             duration_sec = 0
         else:
             duration_sec = int(time_to_seconds(duration_min))
-        views = result["viewCount"]["short"]
-        channel = result["channel"]["name"]
-    return title, duration_min, duration_sec, thumbnail, videoid, views, channel
+    return title, duration_min, duration_sec, thumbnail, videoid
 
 
 def get_yt_info_query_slider(query: str, query_type: int):
