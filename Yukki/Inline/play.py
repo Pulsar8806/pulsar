1fb4e1d69aad89c82041d2d0e36c9c8d6fe68941
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-06 13:41:30 +0300

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -26,14 +26,14 @@ def url_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="Ⓜ️ Fazla Sonuç",
+                text="🔮 Extra Menü",
                 callback_data=f"Search {query}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="👉 Aramayı Kapat",
+                text="👉 Kapat",
                 callback_data=f"forceclose {query}|{user_id}",
             ),
-        ],
+         ],
     ]
     return buttons
 
@@ -42,17 +42,17 @@ def url_markup2(videoid, duration, user_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="🎵 Müzik Dinle",
+                text="🎵 Dinle",
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎥 Videoyu Oynat",
+                text="🎥 İzle",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
         ],
         [
             InlineKeyboardButton(
-                text="👉 Aramayı Kapat",
+                text="👉 Kapat",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
@@ -173,9 +173,9 @@ def secondary_markup(videoid, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="🌀 Diğer Menü", callback_data=f"other {videoid}|{user_id}"
+                text="🔮 Extra Menü", callback_data=f"other {videoid}|{user_id}"
             ),
-            InlineKeyboardButton(text="👉 Menüyü Kapat", callback_data=f"close"),
+            InlineKeyboardButton(text="👉 Kapat", callback_data=f"close"),
         ],
     ]
     return buttons
@@ -190,7 +190,7 @@ def secondary_markup2(videoid, user_id):
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
         [
-            InlineKeyboardButton(text="👉 Menüyü Kapat", callback_data=f"close"),
+            InlineKeyboardButton(text="👉 Kapat", callback_data=f"close"),
         ],
     ]
     return buttons
@@ -215,9 +215,9 @@ def primary_markup(videoid, user_id, current_time, total_time):
         ],
         [
             InlineKeyboardButton(
-                text="🌀 Diğer Menü", callback_data=f"other {videoid}|{user_id}"
+                text="🔮 Extra Menü", callback_data=f"other {videoid}|{user_id}"
             ),
-            InlineKeyboardButton(text="👉 Menüyü Kapat", callback_data=f"close"),
+            InlineKeyboardButton(text="👉 Kapat", callback_data=f"close"),
         ],
     ]
     return buttons
@@ -239,9 +239,9 @@ def timer_markup(videoid, user_id, current_time, total_time):
         ],
         [
             InlineKeyboardButton(
-                text="🌀 Diğer Menü", callback_data=f"other {videoid}|{user_id}"
+                text="🔮 Extra Menü", callback_data=f"other {videoid}|{user_id}"
             ),
-            InlineKeyboardButton(text="👉 Menüyü Kapat", callback_data=f"close"),
+            InlineKeyboardButton(text="👉 Kapat", callback_data=f"close"),
         ],
     ]
     return buttons
@@ -264,7 +264,7 @@ def audio_markup(videoid, user_id, current_time, total_time):
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
-        [InlineKeyboardButton(text="👉 Menüyü Kapat", callback_data=f"close")],
+        [InlineKeyboardButton(text="👉 Kapat", callback_data=f"close")],
     ]
     return buttons
 
@@ -283,7 +283,7 @@ def audio_timer_markup_start(videoid, user_id, current_time, total_time):
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
-        [InlineKeyboardButton(text="👉 Menüyü Kapat", callback_data=f"close")],
+        [InlineKeyboardButton(text="👉 Kapat", callback_data=f"close")],
     ]
     return buttons
 
@@ -296,6 +296,6 @@ audio_markup2 = InlineKeyboardMarkup(
             InlineKeyboardButton(text="⏭️", callback_data=f"skipcb"),
             InlineKeyboardButton(text="⏹️", callback_data=f"stopcb"),
         ],
-        [InlineKeyboardButton("👉 Menüyü Kapat", callback_data="close")],
+        [InlineKeyboardButton("👉 Kapat", callback_data="close")],
     ]
 )
