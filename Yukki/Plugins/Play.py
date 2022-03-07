b08c77cbcebf5fab0f601bfdf90f9ce8d3a08059
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-07 15:38:33 +0300

    Update Play.py

--- Yukki/Plugins/Play.py
+++ Yukki/Plugins/Play.py
@@ -41,7 +41,7 @@ loop = asyncio.get_event_loop()
 
 
 @app.on_message(
-    filters.command(["oynat", f"oynat@{BOT_USERNAME}"]) & filters.group
+    filters.command(["play", f"play@{BOT_USERNAME}"]) & filters.group
 )
 @checker
 @logging
