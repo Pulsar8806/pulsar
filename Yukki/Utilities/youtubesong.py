c547409c62be1f5001414790359eaea8a7cf8ee3
Pulsar554706 <100949028+Pulsar8806@users.noreply.github.com>
2022-03-07 12:43:45 +0300

    Update youtubesong.py

--- Yukki/Utilities/youtubesong.py
+++ Yukki/Utilities/youtubesong.py
@@ -32,7 +32,7 @@ def get_yt_info_query(query: str):
             duration_sec = int(time_to_seconds(duration_min))
         views = result["viewCount"]["short"]
         channel = result["channel"]["name"]
-    return title, duration_min, duration_sec, thumbnail, videoid, views, channel
+    return title, duration_min, duration_sec, thumbnail, videoid
 
 
 
